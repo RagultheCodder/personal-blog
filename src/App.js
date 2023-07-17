8b990252f6a82d49edfed45bbe6ee8fb79e85e7d
Ragul <ragulkumar.shanmuganathan@crystaldelta.com>
2023-07-17 15:24:26 +0530

    add query params to check is exist or not

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useEffect } from 'react';
 import './App.css';
 import Banner from './components/banner/Banner';
 import Skills from './components/skills/Skills';
 
 function App() {
+  useEffect(() => {
+    // Get the URL query string
+    const queryString = window.location.search;
+
+    // Parse the query string to get the parameters
+    const urlParams = new URLSearchParams(queryString);
+
+    console.log(urlParams);
+
+    // Access the individual parameter values
+    const param1 = urlParams.get('orgName');
+    const param2 = urlParams.get('payload');
+    const param3 = urlParams.get('courseData');
+
+    // Access the URL parameters here
+    console.log('param1:', param1);
+    console.log('param2:', param2);
+    console.log('param3:', param3);
+  }, []);
   return (
     <>
       <Banner />
