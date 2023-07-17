dc43e06278ecac733deb65bc757282bc5e187fe5
Ragul <ragulkumar.shanmuganathan@crystaldelta.com>
2023-07-17 15:39:26 +0530

    update query

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import { useEffect } from 'react';
 import './App.css';
 import Banner from './components/banner/Banner';
 import Skills from './components/skills/Skills';
+import { useParams } from 'react-router-dom';
 
 function App() {
+  const { orgName, payload , courseData} = useParams();
+
+  console.log(orgName);
+  console.log(payload);
+  console.log(courseData);
   useEffect(() => {
     // Get the URL query string
     const queryString = window.location.search;
+
+     // Output the entire query string
+     console.log('Query String:', queryString);
 
     // Parse the query string to get the parameters
     const urlParams = new URLSearchParams(queryString);
@@ -22,6 +31,7 @@ function App() {
     console.log('param1:', param1);
     console.log('param2:', param2);
     console.log('param3:', param3);
+
   }, []);
   return (
     <>
